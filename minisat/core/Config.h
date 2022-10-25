62469a3ddbf7263d4a79d25dfd8743c03c26c8ea
Danila-Pechenev <danil-pechenev@mail.ru>
2022-10-25 19:21:30 +0300

    Add config file

---
+++ minisat/core/Config.h
@@ -0,0 +1,8 @@
+// ccmin-mode: 0, 1 or 2
+#define CCMIN_MODE 1
+
+// Define to use back propagation for branching
+#define BACKPROP
+
+// Define to use polarity initialization heuristic
+#define POLARITY_INIT_HEURISTIC
--- minisat/core/Solver.h
+++ minisat/core/Solver.h
@@ -18,11 +18,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
-// Configure the solver
-#define CCMIN_MODE 1
-#define BACKPROP
-#define POLARITY_INIT_HEURISTIC
-// --------------------
+#include <minisat/core/Config.h>
 
 #ifndef Minisat_Solver_h
 #define Minisat_Solver_h
